bf795a727420dc9b43d4b16948d8cc296ec14091
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-11-19 10:31:50 +0200

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import "./globals.css";
+
+const zentry = localFont({
+  src: "../public/fonts/zentry-regular.woff2",
+  variable: "--font-zentry",
+});
+
+const general = localFont({
+  src: "../public/fonts/general.woff2",
+  variable: "--font-general",
+});
+
+const circularWeb = localFont({
+  src: "../public/fonts/circularweb-book.woff2",
+  variable: "--font-circular-web",
+});
+
+const robertMedium = localFont({
+  src: "../public/fonts/robert-medium.woff2",
+  variable: "--font-robert-medium",
+});
+
+const robertRegular = localFont({
+  src: "../public/fonts/robert-regular.woff2",
+  variable: "--font-robert-regular",
+});
+
+export const metadata: Metadata = {
+  title: "Award Winning Website",
+  description:
+    "Award-winning online platform offering the latest computer games, detailed reviews, trailers, gameplay guides, and trusted recommendations to help players choose their next favorite title.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${zentry.variable} ${general.variable} ${circularWeb.variable} ${robertMedium.variable} ${robertRegular.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <div className="container">homepage</div>
+    </main>
+  );
+};
+
+export default Home;
